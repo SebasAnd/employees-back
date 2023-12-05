6b1f5b26d66f5ac47588f0c424b50348941cc20a
Andrés Alejandro Cortés Poveda <andres.alejandro.cortes@correounivalle.edu.co>
2023-12-05 17:57:28 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const chars = require('./admin.js');
+const users = require('./admin.js');
+var bodyParser = require('body-parser');
+
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors');
+
+app.use(bodyParser.json());
+
+app.use('/',chars);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/admin', (req, res) => {
+  res.send('HelloWorld!')
+})
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200, // For legacy browser support
+  methods: "GET, POST"
+}
+
+app.use(cors(corsOptions));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
