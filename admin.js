6bf9c0aa35e774da0089e63aecac84521a889277
Andrés Alejandro Cortés Poveda <and99941@gmail.com>
2023-12-07 17:20:07 -0500

    finished endpoints

--- admin.js
+++ admin.js
@@ -79,17 +79,50 @@ async function VerifyNullBoss(){
     
   }
 }
+router.get('/getUser', async function(req, res) {
+  try{
+      const pool = new Pool(credentials);
+      console.log(req.query.email);
+      const result = await pool.query("SELECT * FROM employees WHERE email='"+req.query.email+"'");
+      const relations = await pool.query("SELECT * FROM relations WHERE boss='"+req.query.email+"' OR employeeref='"+req.query.email+"'");
+      await pool.end();
+      res.setHeader('Content-Type', 'application/json');
 
+      //await VerifyNullBoss();
+      
+      if(result['rows'].length > 0){
+        res.send(JSON.stringify({"employees": result['rows'], "relations": relations['rows']}));
+      }else{
+        res.send(JSON.stringify({result  :{
+          id: -1,
+          name: null,
+          email: null,
+          is_admin: false,
+          nick_name: null,
+          password: null
+  
+        }
+          
+        }));
+        }
+      
+  
+    }catch(err){
+      res.send({'error':err});
+    }
+});
 router.get('/getUsers', async function(req, res) {
     try{
         const pool = new Pool(credentials);
         const result = await pool.query("SELECT * FROM employees");
+        const relations = await pool.query("SELECT * FROM relations");
         await pool.end();
         res.setHeader('Content-Type', 'application/json');
+
         //await VerifyNullBoss();
         
         if(result['rows'].length > 0){
-          res.send(JSON.stringify(result['rows']));
+          res.send(JSON.stringify({"employees": result['rows'], "relations": relations['rows']}));
         }else{
           res.send(JSON.stringify({result  :{
             id: -1,
@@ -143,13 +176,77 @@ router.post('/addUser', async function(req, res) {
 router.post('/updateUser', async function(req, res) {  
   try{
     const pool = new Pool(credentials);//UPDATE public.users    ;
-    const result = await pool.query("UPDATE users SET name='"+req.body.name+"', position='"+req.body.nickname+"'"+
+
+    const versionValue = await pool.query("SELECT * FROM employees WHERE email='"+req.body.email+"'");
+    const result = await pool.query("UPDATE employees SET name='"+req.body.name+"', position='"+req.body.position+"', version="+(versionValue['rows'][0]['version'] + 1)+
         "WHERE email = '"+req.body.email+"'")
 
     const usersRelations = await pool.query("SELECT * FROM relations WHERE boss = '"+req.body.email+"' OR employeeref = '"+req.body.email+"'")
 
     const myBossArray = req.body['relation']["boss"];
     const myEmployeeArray = req.body['relation']["employee"];
+
+    console.log("clicked",myBossArray,myEmployeeArray);
+
+
+
+    for(let i = 0; i< usersRelations['rows'].length;i++){
+      let validateBoss = false;      
+      for(let j=0;j< myBossArray.length;j++){
+        if(usersRelations['rows'][i]['boss'] == myBossArray[j]){
+          validateBoss = true;
+          break;
+        } 
+      }
+      if(validateBoss == false){
+        const addInrelations = await pool.query("DELETE FROM relations WHERE boss='"+usersRelations['rows'][i]['boss']+"' AND employeeref ='"+req.body.email+"'");
+        console.log(addInrelations);                
+      }
+    }
+
+    for(let i = 0; i< usersRelations['rows'].length;i++){
+      let validateEmployee = false;      
+      for(let j=0;j< myEmployeeArray.length;j++){
+        if(usersRelations['rows'][i]['employeeref'] == myEmployeeArray[j]){
+          validateEmployee = true;
+          break;
+        } 
+      }
+      if(validateEmployee == false){
+        const addInrelations = await pool.query("DELETE FROM relations WHERE boss='"+req.body.email+"' AND employeeref ='"+usersRelations['rows'][i]['employeeref']+"'");
+        console.log(addInrelations);                
+      }
+    }
+
+    for(let m = 0; m < myBossArray.length; m++){
+      let validateIn = false;
+      for(let n = 0;n < usersRelations['rows'].length; n++){
+        if(myBossArray[m] == usersRelations['rows'][n]['boss']){
+          validateIn = true;
+          break;
+        }
+      }
+      if(validateIn == false){
+        const res = await pool.query("INSERT INTO relations(boss, employeeref)  VALUES ('"+myBossArray[m]+"', '"+req.body.email+"');");  
+        console.log(res);     
+      }
+    }
+
+    for(let m = 0; m < myEmployeeArray.length; m++){
+      let validateIn = false;
+      for(let n = 0;n < usersRelations['rows'].length; n++){
+        if(myEmployeeArray[m] == usersRelations['rows'][n]['employeeref']){
+          validateIn = true;
+          break;
+        }
+      }
+      console.log(myEmployeeArray[m],req.body.email,validateIn);
+      if(validateIn == false){
+        const res= await pool.query("INSERT INTO relations(boss, employeeref)  VALUES ('"+req.body.email+"', '"+myEmployeeArray[m]+"');");   
+        console.log(res);   
+      }
+    }
+    VerifyNullBoss();
     
     await pool.end();
     res.setHeader('Content-Type', 'application/json');
@@ -164,13 +261,14 @@ router.post('/updateUser', async function(req, res) {
 });
 router.post('/deleteUser', async function(req, res) {  
   try{
-    console.log(req.body)
     const pool = new Pool(credentials);//UPDATE public.users    ;
-    const result = await pool.query("DELETE FROM users WHERE email='"+req.body.email+"'");
+    const result = await pool.query("DELETE FROM relations WHERE boss='"+req.body.email+"' OR employeeref='"+req.body.email+"'");
+    const delUser = await pool.query("DELETE FROM employees WHERE email='"+req.body.email+"'");
+    VerifyNullBoss();
     await pool.end();
     res.setHeader('Content-Type', 'application/json');
     
-      res.send(JSON.stringify(result));
+      res.send(JSON.stringify(delUser));
     
 
   }catch(err){
